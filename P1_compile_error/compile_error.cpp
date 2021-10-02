5d0ecab62b4fd64cd215dd50dda0141a78640f52
Twicster <mathias.duedahl@gmail.com>
2021-10-01 17:01:27 -0700

    Compilation errors all activated, txt document added with a description of errors introduced.

--- P1_compile_error/compile_error.cpp
+++ P1_compile_error/compile_error.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 using namespace std;
 
-int main()  //Remove main
+//int main()  //Remove main
 {
 	//Removed variable declarations
 	int numberSurveyed;
@@ -12,13 +12,13 @@ int main()  //Remove main
 	int likeAtHome;
 
 	cout << "How many students were surveyed? ";
-	cin >> numberSurveyed;	//Forget semicolon 
+	cin >> numberSurveyed//;	//Forget semicolon 
 	cout << "How many of them prefer being at school in person? ";
 	cin >> likeInPerson;
 	cout << "How many of them would rather do school from home? ";
 	cin >> likeAtHome;
 
-	double pctLikeInPerson = 100.0 * likeInPerson / numberSurveyed;	//Leave out type declaration diff with initialization?
+	/*double*/ pctLikeInPerson = 100.0 * likeInPerson / numberSurveyed;	//Leave out type declaration
 	double pctLikeAtHome = 100.0 * likeAtHome / numberSurveyed;
 
 	cout.setf(ios::fixed);
@@ -28,7 +28,7 @@ int main()  //Remove main
 	cout << pctLikeInPerson << "% prefer being at school in person." << endl;
 	cout << pctLikeAtHome << "% prefer doing school from home." << endl;
 
-	if (likeInPerson > likeAtHome) //Leave out parenthesis
+	if /*(*/ likeInPerson > likeAtHome/*)*/ //Leave out parenthesis
 		cout << "More students like being at school in person than doing school at home." << endl;
 	else
 		cout << "More students like doing school from home than being at school in person." << endl;
